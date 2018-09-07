4247fb3ca7de43e709e28ad5c8af5882cebddc0f
Brent Woodruff <brent@fprimex.com>
2018-09-07 12:05:46 -0400

    Add Xamarin.WPF project

---
+++ FormTest.WPF/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace FormTest.WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
