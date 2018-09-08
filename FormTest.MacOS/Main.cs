a884594ac335958d38a29765df3ee30d710351cd
Brent Woodruff <brent@fprimex.com>
2018-09-08 14:02:17 -0400

    add working MacOS version

---
+++ FormTest.MacOS/Main.cs
@@ -0,0 +1,15 @@
+﻿using AppKit;
+
+namespace FormTest.MacOS
+{
+    static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            NSApplication.Init();
+            NSApplication.SharedApplication.Delegate = (AppKit.INSApplicationDelegate)new AppDelegate();
+            NSApplication.SharedApplication.MainMenu = new NSMenu();
+            NSApplication.Main(args);
+        }
+    }
+}
